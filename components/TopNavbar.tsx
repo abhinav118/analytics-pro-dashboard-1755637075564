0da7dad2c324818df43b8d66f3bbda777d0e773d
Abhi <abhinav118@gmail.com>
2025-08-19 16:58:04 -0400

    Add components/TopNavbar.tsx

---
+++ components/TopNavbar.tsx
@@ -0,0 +1,13 @@
+interface TopNavbarProps {
+  title?: any
+  showSearch?: any
+  showProfile?: any
+}
+
+export default function TopNavbar({ title, showSearch, showProfile }: TopNavbarProps) {
+  return (
+    <div className="bg-white shadow-sm border-b border-gray-200 px-6 py-4 flex items-center justify-between">
+      {/* TopNavbar component content */}
+    </div>
+  )
+}
