cb5f62abd8e3bd892635b3bedea41f26f967ea70
Abhi <abhinav118@gmail.com>
2025-08-19 16:58:03 -0400

    Add components/ChartContainer.tsx

---
+++ components/ChartContainer.tsx
@@ -0,0 +1,13 @@
+interface ChartContainerProps {
+  title?: any
+  type?: any
+  data?: any
+}
+
+export default function ChartContainer({ title, type, data }: ChartContainerProps) {
+  return (
+    <div className="bg-white rounded-lg shadow-sm p-6">
+      {/* ChartContainer component content */}
+    </div>
+  )
+}
